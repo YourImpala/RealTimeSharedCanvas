85a9d76e53b3d6dd2063f68648daaa926f13a823
YourImpala <dmitriy.petrov.1993@mail.ru>
2019-08-17 18:15:15 +0300

    Added canvas rendering for new connections

--- public/script.js
+++ public/script.js
@@ -3,8 +3,15 @@ function setup(){
 
     //receives coordinates from the server
     socket.on('data', anotherClientDrawing);
+    socket.on('canvasData',newConnectionDraw);
 }
-
+//draws the contents of the canvas for a new connection
+function newConnectionDraw(data) {
+    for (i=0;i<data.length;i++) {
+        fill(data[i].color1, data[i].color2, data[i].color3);
+        ellipse(data[i].x, data[i].y, 80, 80);
+    }
+}
 //draws on this client
 function draw() {
     if (mouseIsPressed && mouseButton === LEFT) {
@@ -17,7 +24,10 @@ function draw() {
 
         let data = {
             x: mouseX,
-            y: mouseY
+            y: mouseY,
+            color1: color1,
+            color2: color2,
+            color3: color3
         }
         //send coordinates to server
         socket.emit('data', data);
@@ -28,11 +38,7 @@ function draw() {
 
 //draws on another client
 function anotherClientDrawing(data) { 
-    let color1 = Math.floor(Math.random() * 256);
-    let color2 = Math.floor(Math.random() * 256);
-    let color3 = Math.floor(Math.random() * 256);
-    
-    fill(color1, color2, color3);
+    fill(data.color1, data.color2, data.color3);
     ellipse(data.x, data.y, 80, 80);
 }
 
--- server.js
+++ server.js
@@ -7,10 +7,15 @@ const server = app.listen(port);
 app.use(express.static('public'));
 
 const io = socket(server);
-
+//canvas data storage
+let canvasData = [];
 io.on('connection', socket => {
+    console.log(`New connection : ${socket.id}`)
+    //sends canvas data to all new connections
+    io.sockets.emit('canvasData', canvasData);
     //receives data from the client
     socket.on('data', data => {
+        canvasData.push(data);
         //send coordinates to another clients
         socket.broadcast.emit('data', data);
     });
